8452580161e698ef28976584df1e7a3c350e3b55
James Gilbert <James.gilbert@nottingham.ac.uk>
2018-12-06 17:26:40 +0000

    War

--- test.py
+++ test.py
@@ -19,15 +19,16 @@ if not conf:
     screen(office, ["You have selected GLOBAL THERMO NUCLEAR WAR"])
     click.pause()
 
+    screen(bomb, [
+            "Nuclear war has ravaged the earth for nearly three decades",
+            "Nobody knows how  or why the first bomb was dropped.",
+            "You are one of the few remaining humans",
+            ])
+    click.pause()
+    screen(bomb,
+           ["The machines have lost.", "We are near there final base",
+            "However, we have reason to believe that they are plotting something big"]
+    )
+
 screen(office,[])
 processing("Loading...")
-
-screen(bomb, [
-        "Nuclear war has ravaged the earth for nearly three decades",
-        "You are one of the few million surviros left",
-        "Nobody knows how the first bomb was dropped."])
-click.pause()
-screen(bomb,
-       ["The machines have lost.", "We are near there final base",
-        "However, we have reason to believe that they are plotting something big"]
-)
